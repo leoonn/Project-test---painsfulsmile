9971a9cddcb88032bfda545d4e0edede7c0a008b
leoonn <flateia13@hotmail.com>
2020-09-11 22:51:36 -0300

    startbutton

--- Test-painsfulsmile/Assets/Scripts/Game Manager/Menu.cs
+++ Test-painsfulsmile/Assets/Scripts/Game Manager/Menu.cs
@@ -27,7 +27,7 @@ public class Menu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Loading");
+        SceneManager.LoadScene("GameScene");
         TimeGameSession();
         TimeSpawnEnemy();
     }
